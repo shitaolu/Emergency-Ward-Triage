b6966c9df6db1b454572e97f180789f8f42f5657
shitaolu <37382425+shitaolu@users.noreply.github.com>
2018-11-15 23:32:39 -0500

    Add files via upload

--- APQ.java
+++ APQ.java
@@ -1,9 +1,9 @@
-package A2Q2S;
+package A2Q2;
 
 import java.util.*;
 
 /**
- * Adaptable priority queue using location-aware entries in a min-heap, based on
+ * Adaptible priority queue using location-aware entries in a min-heap, based on
  * an extendable array.  The order in which equal entries were added is preserved.
  *
  * @author jameselder
@@ -20,7 +20,7 @@ public class APQ<E> {
      * @param comparator used to compare the entries
      * @param locator used to locate the entries in the queue
      * @throws NullPointerException if comparator or locator parameters are null
-    */
+     */
     public APQ(Comparator<E> comparator, Locator<E> locator) throws NullPointerException {
         if (comparator == null || locator == null) {
             throw new NullPointerException();
@@ -29,6 +29,7 @@ public class APQ<E> {
         apq.add(null); //dummy value at index = 0
         this.comparator = comparator;
         this.locator = locator;
+
     }
 
     /**
@@ -38,12 +39,15 @@ public class APQ<E> {
      * @throws NullPointerException if parameter e is null
      */
     public void offer(E e) throws NullPointerException {
-        if (e == null) {
-            throw new NullPointerException();
-        }
-        apq.add(e); // append to array
-        locator.set(e, size()); //update location
-        upheap(size()); //move to correct location in heap
+    	if (e == null){
+    		throw new NullPointerException();
+    	}
+    	apq.add(e);
+    	locator.set(e, apq.size()-1);
+    	if(apq.size()>2){
+    		upheap(locator.get(e)); 
+    	}
+    	 
     }
 
    /**
@@ -52,57 +56,51 @@ public class APQ<E> {
      * @param pos the location of the entry to remove
      * @throws BoundaryViolationException if pos is out of range
      */
-    public void remove(int pos) throws BoundaryViolationException {
-        E e;
-        if (pos < 1 || pos > size()) {
-            throw new BoundaryViolationException();
-        }
+	public void remove(int pos) throws BoundaryViolationException {
+		if (pos > apq.size()-1 || pos<=0) {
+			throw new BoundaryViolationException();
+		}
+		else if(pos == apq.size()-1){   
+			apq.remove(apq.size() - 1);
+		}
+		else{
+			apq.set(pos, apq.get(apq.size()-1));
+			locator.set(apq.get(apq.size()-1), pos);
+			apq.remove(apq.size()-1);
+			if (2 * pos <(apq.size()-1)) {     //左右都有child
+				if (comparator.compare(apq.get(pos / 2), apq.get(pos)) > 0) {
+					upheap(pos);
+				} 
+				else if (comparator.compare(apq.get(pos), apq.get(pos * 2)) > 0
+						|| comparator.compare(apq.get(pos), apq.get(pos * 2 + 1)) > 0) {
+					downheap(pos);}
+			}
+			
+			else if(2*pos == ( apq.size()-1)){  //只有一个left child
+				downheap(pos);
+			}
 
-        locator.set(apq.get(pos), 0); //null location of removed entry
-        e = apq.remove(size()); //remove last entry
-        if (pos != size()+1) { //if element to be removed was not last
-            apq.set(pos, e); //store last entry at pos
-            locator.set(e, pos); //update location
-            downheap(pos); //move to correct location
-        }
-    }
-
-
-   /**
-     * Removes the entry at the specified location.
-     *
-     * @param e1 the entry to remove
-     */
-    public void remove(E e1) {
-        E e2;
-        int pos;
-        pos = locator.get(e1); //get location of entry to be removed
-        locator.set(e1, 0); //null location of removed entry
-        e2 = apq.remove(size()); //remove last entry
-        if (pos != size() + 1) { //if element to be removed was not last
-            apq.set(pos, e2); //store last entry at pos
-            locator.set(e2, pos); //update location
-            downheap(pos); //move to correct location
-        }
-    }
+			else if(2*pos > (apq.size()-1)){
+				upheap(pos);
+			}
+		}
+		
+	}
 
    /**
      * Removes the first entry in the priority queue.
      */
     public E poll() {
-        E e1, e2;
-        if (isEmpty()) {
-            return null;
-        }
-        e1 = apq.get(1); //Get first entry
-        e2 = apq.remove(size()); //Remove last entry
-        if (size() > 0) { //Otherwise last entry was first entry
-            apq.set(1, e2); //Store removed last entry at first location
-            locator.set(e2, 1); //Update location
-            downheap(1); //Move to correct location
-        }
-        locator.set(e1, 0); //Zero location of removed entry
-        return e1;
+    	if (isEmpty()){
+    		return null;
+    	}
+    	E temp = apq.get(1);
+    	apq.set(1, apq.get(apq.size()-1));
+    	locator.set(apq.get(apq.size()-1), 1);
+    	apq.remove(apq.size()-1);
+    	downheap(1);
+    	return temp;
+
     }
 
   /**
@@ -128,37 +126,52 @@ public class APQ<E> {
      * Shift the entry at pos upward in the heap to restore the minheap property
      * @param pos the location of the entry to move
      */
-    private void upheap(int pos) {
-        int parentPos;
-        if (pos > 1) { //Otherwise already at top
-            parentPos = pos / 2;
-            if (comparator.compare(apq.get(pos), apq.get(parentPos)) < 0) { //do not move if tie.
-                swap(pos, parentPos);
-                upheap(parentPos);
-            }
-        }
-    }
+    private void upheap(int pos) { 
+    		while(pos >= 2 && comparator.compare(apq.get(pos/2), apq.get(pos)) > 0){
+        		swap(pos,pos/2);
+        		pos = pos/2;
+        	}
+    	}
+
+   
 
     /**
      * Shift the entry at pos downward in the heap to restore the minheap property
      * @param pos the location of the entry to move
      */
-    private void downheap(int pos) {
-        int childPos, rightChildPos;
-        childPos = 2 * pos; //left child
-        if (childPos <= size()) { //Otherwise already at bottom.
-            rightChildPos = 2 * pos + 1;
-            if (rightChildPos <= size()) { //right child exists
-                if (comparator.compare(apq.get(childPos), apq.get(rightChildPos)) > 0) { //choose left on tie
-                    childPos = rightChildPos; //right child is smaller than left
-                }
-            }
-            if (comparator.compare(apq.get(pos), apq.get(childPos)) >= 0) { //move down on tie
-                swap(pos, childPos);
-            }
-            downheap(childPos);
-        }
-    }
+	private void downheap(int pos) {
+		// 三种情况，第一种，左边没有，右边也没有，那就什么都不要做,因为本身就已经在最底了
+		// 第二种，左边有，右边没有
+		// 第三种，左边有，右边有
+
+		while (2 * pos < apq.size()-1) { //有两个child
+			if (comparator.compare(apq.get(pos), apq.get(pos * 2)) > 0) { //parent 大于 left child
+				if (comparator.compare(apq.get(2 * pos), apq.get(2 * pos + 1)) >= 0) { //比较左边和右边，如果左边大，和右边换
+					swap(pos, pos * 2 + 1);
+					pos = pos * 2 + 1;
+				} 
+				else if (comparator.compare(apq.get(2 * pos), apq.get(2 * pos + 1)) < 0) {//如果右边比较大，和左边换
+					swap(pos, pos * 2);
+					pos = pos * 2;
+				}
+			} 
+			
+			else if (comparator.compare(apq.get(pos), apq.get(pos * 2)) <= 0) { // parent 小于 left child
+				if (comparator.compare(apq.get(pos), apq.get(pos * 2 + 1)) > 0) { //parent 大于 right child,交换右边的
+					swap(pos, pos * 2 + 1);
+					pos = pos * 2 + 1;
+				}
+			}
+		}
+		
+		if (2 * pos == apq.size()-1) { //只有left child
+			if (comparator.compare(apq.get(pos), apq.get(pos * 2)) >= 0) {
+				swap(pos, pos * 2);
+				pos = pos * 2;
+			}
+		}
+	}
+    	
 
     /**
      * Swaps the entries at the specified locations.
@@ -167,13 +180,14 @@ public class APQ<E> {
      * @param pos2 the location of the second entry 
      */
     private void swap(int pos1, int pos2) {
-        E e1, e2;
-        
-        e1 = apq.get(pos1);
-        e2 = apq.get(pos2);
-        apq.set(pos1, e2);
-        locator.set(e2, pos1); //update location
-        apq.set(pos2, e1);
-        locator.set(e1, pos2); //update location
+    	E temp1 = apq.get(pos1);
+    	E temp2 = apq.get(pos2);
+    	locator.set(temp1, pos2);
+    	locator.set(temp2, pos1);
+    	apq.set(pos1, temp2);
+    	apq.set(pos2, temp1);
+    	
+        //implement this method
     }
+    
 }
