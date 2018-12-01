82450cb6f71b224c9fda6cd039b50d123eff8d56
shitaolu <37382425+shitaolu@users.noreply.github.com>
2018-11-30 23:16:38 -0500

    Add files via upload

--- testPatientTriage.java
+++ testPatientTriage.java
@@ -1,31 +1,89 @@
-package A2Q2S;
+package A2Q2;
 
 /**
  * Example test program for PatientTriage
  * @author elder
  */
 public class testPatientTriage {
-    public static void main(String[] args) throws BoundaryViolationException, EmptyQueueException {
-        String patient;
-        
-        PatientTriage patientTriage = new PatientTriage(new Time (3, 0)); //Time limit of 3 hours
-        patientTriage.add(new Patient(1, 4, new Time (0, 30)));
-        patientTriage.add(new Patient(2, 3, new Time (1, 45)));
-        patientTriage.add(new Patient(3, 2, new Time (2, 45)));
-        patient = patientTriage.remove(new Time (3, 0)).toString(); 
+	public static void main(String[] args) throws BoundaryViolationException, EmptyQueueException {
+		String patient;
+		long startTime = System.nanoTime();
+		PatientTriage patientTriage;
+		
+		patientTriage = new PatientTriage(new Time(3, 0));
+		patientTriage.add(new Patient(1, 3, new Time(0, 30)));
+		patientTriage.add(new Patient(7, 6, new Time(0, 50)));
+		patientTriage.add(new Patient(2, 2, new Time(1, 30)));
+		patientTriage.add(new Patient(6, 5, new Time(1, 45)));
+		patientTriage.add(new Patient(3, 4, new Time(2, 15)));
+		patientTriage.add(new Patient(4, 1, new Time(2, 50)));
+		patientTriage.add(new Patient(5, 7, new Time(3, 20)));
+		patientTriage.add(new Patient(8, 8, new Time(3, 40)));
+		
+		//case 1
+		patient = patientTriage.remove(new Time (2, 0)).toString(); 
         System.out.print("Now seeing: ");
-        System.out.println(patient); //Should be patient 3
-        patient = patientTriage.remove(new Time (4, 15)).toString();
-        System.out.print("Now seeing: "); //Should be patient 1
         System.out.println(patient);
-        patient = patientTriage.remove(new Time (4, 30)).toString();
-        System.out.print("Now seeing: "); //Should be patient 2
+		System.out.println("correct: patient 4");
+		
+		//case 2
+		patient = patientTriage.remove(new Time (3, 0)).toString(); 
+        System.out.print("Now seeing: ");
         System.out.println(patient);
-        Patient p1 = new Patient(1000, 1, new Time(0, 0));
-	Patient p2 = new Patient(2000, 2, new Time(0, 0));
-	PatientPriorityComparator comparator = new PatientPriorityComparator();
-	System.out.print(comparator.compare(p1, p2)); // Failure. Expected <-1> but was: <1>
+		System.out.println("correct: patient 2");
 
-    }
-    
+		//case 3
+		patientTriage.add(new Patient(10, 1, new Time(4, 10)));
+		System.out.println();
+		patient = patientTriage.remove(new Time (4,10)).toString(); 
+        System.out.print("Now seeing: ");
+        System.out.println(patient);
+		System.out.println("correct: patient 1");
+		
+		//case 4
+		patient = patientTriage.remove(new Time (3, 45)).toString(); 
+        System.out.print("Now seeing: ");
+        System.out.println(patient);
+		System.out.println("correct: patient 10");
+		
+		//case 5
+		patient = patientTriage.remove(new Time (4,20)).toString(); 
+        System.out.print("Now seeing: ");
+        System.out.println(patient);
+		System.out.println("correct: patient 7");
+
+		//case 6
+		patient = patientTriage.remove(new Time (5,00)).toString(); 
+        System.out.print("Now seeing: ");
+        System.out.println(patient);
+		System.out.println("correct: patient 6");
+		
+		//case 7
+		patient = patientTriage.remove(new Time (5,5)).toString(); 
+        System.out.print("Now seeing: ");
+        System.out.println(patient);
+		System.out.println("correct: patient 3");
+		
+		patientTriage.add(new Patient(20, 5, new Time(3, 1)));
+		
+		//case 8
+		patient = patientTriage.remove(new Time (5,55)).toString(); 
+        System.out.print("Now seeing: ");
+        System.out.println(patient);
+		System.out.println("correct: patient 20");
+		
+		//case 9
+		PatientTriage patientTriage2 = new PatientTriage(new Time(3, 0));
+		patientTriage2.add(new Patient(1, 3, new Time(1,20)));
+		patientTriage2.add(new Patient(2, 4, new Time(0,20)));
+		
+		patient = patientTriage2.remove(new Time (3,30)).toString(); 
+        System.out.print("Now seeing: ");
+        System.out.println(patient);
+		System.out.println("correct: patient 2");
+		
+		long stopTime = System.nanoTime();
+        double elapsedTime = (double) (stopTime - startTime) / 1000000; //in msec
+        System.out.println("Execution time: " + elapsedTime);
+	}
 }
